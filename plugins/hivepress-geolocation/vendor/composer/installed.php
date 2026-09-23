1387781fa2676057e20e14d6d937191df01ae2e4
Pashtet221 <90337059+Pashtet221@users.noreply.github.com>
2026-09-23 21:46:36 +0300

    3
    3

---
+++ plugins/hivepress-geolocation/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.3.10',
+        'version' => '1.3.10.0',
+        'reference' => '930a4a00d6d7ae5187eae078579e673dc91871ba',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.3.10',
+            'version' => '1.3.10.0',
+            'reference' => '930a4a00d6d7ae5187eae078579e673dc91871ba',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
